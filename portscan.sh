2510f4421ba9deae4a418de795378870371a0d1a
SecurityForUs <ehansen@securityfor.us>
2012-12-14 16:05:33 -0800

    cool stuff yo

---
+++ portscan.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Code is swipped from http://legroom.net/2010/05/02/port-testing-and-scanning-bash
+#
+# Usage: ./portscan.sh
+#
+# Change range in {...} block as you please.
+
+function port() {
+        (echo > /dev/tcp/$1/$2) &> /dev/null
+        if [ $? -eq 0 ]; then
+                echo "$1:$2 open"
+        else
+                echo "$1:$2 closed"
+        fi
+}
+
+for i in {22..80}; do
+        port 192.168.1.13 $i
+done
